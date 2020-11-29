76033b73ce2ee91977db72463577bea43d740558
davas93 <vasin.da93@gmail.com>
2020-11-29 15:20:45 +0300

    complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let week = [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+  ],
+  toDay = new Date().getDay();
+
+for (let i = 0; i < week.length; i++) {
+  if (i === 5 || i === 6) {
+    week[i] = week[i].italics();
+  }
+  if (i === toDay) {
+    let correctIndex = i == 0 ? 6 : i - 1;
+    week[correctIndex] = week[correctIndex].bold();
+  }
+}
+document.write(week.join("<br>"));
