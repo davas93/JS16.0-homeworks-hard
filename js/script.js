5afbd403a30d9cb024696eb5540f964d04a124c9
davas93 <vasin.da93@gmail.com>
2020-12-01 19:24:05 +0300

    checkpoint2

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ let date1 = new Date(),
   ];
 
 date1 = [
-  "Сегодня ",
+  "Сегодня",
   weekday[date1.getDay()],
   date1.getDate(),
   month[date1.getMonth()],
@@ -38,5 +38,13 @@ date1 = [
   "секунд",
 ];
 
+let lastHoursNum = date1[6].toString().split("").pop(),
+  lastMinutesNum = date1[8].toString().split("").pop(),
+  lastSecondsNum = date1[10].toString().split("").pop(),
+  hoursText = date1[7],
+  minutesText = date1[9],
+  secondsText = date1[11];
+
 document.write(date1);
 console.log(date1);
+console.log(minutesText);
