d080bfc6e13726de030279efd4ba49f181e58d3e
davas93 <vasin.da93@gmail.com>
2020-12-02 08:55:54 +0300

    checkpoint1

--- js/script.js
+++ js/script.js
@@ -1,109 +1,57 @@
-"use strict";
+function clock() {
+  let d = new Date(),
+    year = d.getFullYear(),
+    monthNum = d.getMonth(),
+    weekday = d.getDay(),
+    day = d.getDate(),
+    hours = d.getHours(),
+    minutes = d.getMinutes(),
+    seconds = d.getSeconds();
 
-//Задача вариант а)
-let date1 = new Date(),
-  weekday = [
-    "Воскресенье",
+  month = new Array(
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря"
+  );
+
+  week = new Array(
+    "Восересенье",
     "Понедельник",
     "Вторник",
     "Среда",
     "Четверг",
     "Пятница",
-    "Суббота",
-  ],
-  month = [
-    "Января",
-    "Февраля",
-    "Марта",
-    "Апреля",
-    "Мая",
-    "Июня",
-    "Июля",
-    "Августа",
-    "Сентября",
-    "Октября",
-    "Ноября",
-    "Декабря",
+    "Суббота"
+  );
+
+  dateTime = [
+    "Сегодня ",
+    week[weekday],
+    ", " + day + " " + month[monthNum],
+    " ",
+    year,
+    " года, ",
+    hours,
+    " час ",
+    minutes,
+    " минут ",
+    seconds,
+    " секунд",
   ];
-
-date1 = [
-  "Сегодня",
-  weekday[date1.getDay()] + ",",
-  date1.getDate(),
-  month[date1.getMonth()],
-  date1.getFullYear(),
-  "года" + ",",
-  date1.getHours(),
-  "час",
-  date1.getMinutes(),
-  "минут",
-  date1.getSeconds(),
-  "секунд",
-];
-date1[10] = 11;
-let lastHoursNum = date1[6].toString().split("").pop(),
-  lastMinutesNum = date1[8].toString().split("").pop(),
-  lastSecondsNum = date1[10].toString().split("").pop();
-
-/*//Склоняем часы
-if (lastHoursNum == "2" || lastHoursNum == "3" || lastHoursNum == "4") {
-  date1[7] = "часа";
-} else if (
-  lastHoursNum == "0" ||
-  lastHoursNum == "5" ||
-  lastHoursNum == "6" ||
-  lastHoursNum == "7" ||
-  lastHoursNum == "8" ||
-  lastHoursNum == "9"
-) {
-  date1[7] = "часов";
-} else {
-  date1[7] = "час";
+  if (document.layers) {
+    document.layers.doc_time.document.write(dateTime);
+    document.layers.doc_time.document.close();
+  } else document.getElementById("doc_time").innerHTML = dateTime.join(" ");
+  setInterval("clock()", 1000);
 }
-
-//Cклоняем минуты
-if (lastMinutesNum == "2" || lastMinutesNum == "3" || lastMinutesNum == "4") {
-  date1[9] = "минуты";
-} else if (
-  lastMinutesNum == "0" ||
-  lastMinutesNum == "5" ||
-  lastMinutesNum == "6" ||
-  lastMinutesNum == "7" ||
-  lastMinutesNum == "8" ||
-  lastMinutesNum == "9"
-) {
-  date1[9] = "минут";
-} else {
-  date1[9] = "минута";
-}
-
-//Cклоняем секунды
-if (lastSecondsNum == "2" || lastSecondsNum == "3" || lastSecondsNum == "4") {
-  date1[11] = "секунды";
-} else if (
-  lastSecondsNum == "0" ||
-  lastSecondsNum == "5" ||
-  lastSecondsNum == "6" ||
-  lastSecondsNum == "7" ||
-  lastSecondsNum == "8" ||
-  lastSecondsNum == "9"
-) {
-  date1[11] = "секунд";
-} else {
-  date1[11] = "секунда";
-}*/
-if (10 <= date1[10] <= 20) {
-  date1[11] = "секунд";
-}
-if (["2", "3", "4"].indexOf(lastSecondsNum) > -1) {
-  date1[11] = "секунды";
-} else if ("1" === lastSecondsNum) {
-  date1[11] = "секунда";
-} else {
-  date1[11] = "секунд";
-}
-
-document.write(date1.join(" "));
-
-//Задача вариант б)
-console.log(lastSecondsNum);
+clock();
+console.log(dateTime);
